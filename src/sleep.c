e138489e5ee96016d404ca509f05a606fafd73ea
Nick Krecklow <hello@github.nklow.com>
2023-05-12 12:18:20 -0500

    expose time utils via sleep.h

--- src/sleep.c
+++ src/sleep.c
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include <time.h>
 
-static inline void markTime(struct timespec *spec) {
+void timeGetNow(struct timespec *spec) {
     clock_gettime(CLOCK_MONOTONIC_RAW, spec);
 }
 
-static inline long timeMillisElapsed(struct timespec start,
-                                     struct timespec end) {
-    return (long) ((end.tv_sec - start.tv_sec) * 1000 +
-                   (end.tv_nsec - start.tv_nsec) / 1000000);
+long timeMillisElapsed(const struct timespec *start,
+                       const struct timespec *end) {
+    return (long) ((end->tv_sec - start->tv_sec) * 1000 +
+                   (end->tv_nsec - start->tv_nsec) / 1000000);
 }
 
-static inline void setTimeMillis(struct timespec *spec, long millis) {
+void timeSetMillis(struct timespec *spec, long millis) {
     spec->tv_sec = millis / 1000;
     spec->tv_nsec = (millis % 1000) * 1000000;
 }
@@ -22,19 +22,19 @@ static inline void setTimeMillis(struct timespec *spec, long millis) {
 static inline void sleepTimer(long millis, long *latency) {
     struct timespec start, end;
 
-    markTime(&start);
+    timeGetNow(&start);
 
     struct timespec t;
-    setTimeMillis(&t, millis);
+    timeSetMillis(&t, millis);
 
     if (nanosleep(&t, NULL) != 0) perror("error while invoking nanosleep");
 
-    markTime(&end);
+    timeGetNow(&end);
 
     // this offsets the timing of the next frame by the difference in duration of
     //  the actual sleep time vs the expected sleep time
     // TODO: there are better ways to handle this using stdev
-    *latency = millis - timeMillisElapsed(start, end);
+    *latency = millis - timeMillisElapsed(&start, &end);
 }
 
 void sleepTimerLoop(sleep_fn_t sleep, long millis) {
@@ -43,15 +43,15 @@ void sleepTimerLoop(sleep_fn_t sleep, long millis) {
     long latency = 0;
 
     while (true) {
-        markTime(&start);
+        timeGetNow(&start);
 
         const bool doContinue = sleep();
         if (!doContinue) break;
 
-        markTime(&end);
+        timeGetNow(&end);
 
         const long remainingTime =
-                millis - timeMillisElapsed(start, end) + latency;
+                millis - timeMillisElapsed(&start, &end) + latency;
 
         if (remainingTime > 0) sleepTimer(remainingTime, &latency);
     }
--- src/sleep.h
+++ src/sleep.h
@@ -2,6 +2,14 @@
 #define FPLAYER_SLEEP_H
 
 #include <stdbool.h>
+#include <time.h>
+
+void timeGetNow(struct timespec *spec);
+
+long timeMillisElapsed(const struct timespec *start,
+                       const struct timespec *end);
+
+void timeSetMillis(struct timespec *spec, long millis);
 
 typedef bool (*sleep_fn_t)(void);
 
