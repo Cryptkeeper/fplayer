785feed92b18f6e525d5807e642040b35c32d6bc
Nick Krecklow <hello@git.nklow.com>
2024-03-18 20:18:05 -0500

    fix memory leak in dsprintf test

--- test/dsprintf.c
+++ test/dsprintf.c
@@ -1,15 +1,23 @@
 #undef NDEBUG
 #include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "std/string.h"
 
+#define assert_strcmp(s, v)                                                    \
+    do {                                                                       \
+        char *p = s;                                                           \
+        assert(strcmp(p, v) == 0);                                             \
+        free(p);                                                               \
+    } while (0)
+
 int main(__attribute__((unused)) int argc,
          __attribute__((unused)) char **argv) {
-    assert(strcmp(dsprintf("%s", "hello"), "hello") == 0);
-    assert(strcmp(dsprintf("hello %s", "world"), "hello world") == 0);
-    assert(strcmp(dsprintf("%s world", "hello"), "hello world") == 0);
-    assert(strcmp(dsprintf("%s %s", "hello", "world"), "hello world") == 0);
+    assert_strcmp(dsprintf("%s", "hello"), "hello");
+    assert_strcmp(dsprintf("hello %s", "world"), "hello world");
+    assert_strcmp(dsprintf("%s world", "hello"), "hello world");
+    assert_strcmp(dsprintf("%s %s", "hello", "world"), "hello world");
 
     return 0;
 }
