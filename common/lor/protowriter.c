c54d8244d8b536cc2a92b2d75aa4b19cb8f9b35d
Nick Krecklow <hello@github.nklow.com>
2024-05-19 08:50:30 -0500

    allocate LorBuffer struct with calloc
    Housekeeping change, the struct fields are manually zeroed already.

--- common/lor/protowriter.c
+++ common/lor/protowriter.c
@@ -8,7 +8,7 @@
 #define LOR_BUFFER_SIZE 256
 
 struct LorBuffer* LB_alloc(void) {
-    struct LorBuffer* lb = malloc(sizeof(struct LorBuffer));
+    struct LorBuffer* lb = calloc(1, sizeof(struct LorBuffer));
     if (lb == NULL) return NULL;
     if ((lb->buffer = calloc(LOR_BUFFER_SIZE, 1)) == NULL) {
         LB_free(lb);
